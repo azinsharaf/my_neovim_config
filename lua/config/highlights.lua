25a13a763cf4307274503af62d8578321d69f2a2
Azin Sharaf <azin.sharaf@icloud.com>
2024-01-04 11:12:20 -0800

    highlights

---
+++ lua/config/highlights.lua
@@ -0,0 +1,7 @@
+-- Set the global background color
+vim.cmd([[ highlight Normal guibg=rgba(0,0,0,0.9) ]])
+
+-- Set the background for Telescope
+-- vim.cmd([[highlight TelescopeNormal guibg=NONE ctermbg=NONE]])
+
+-- Define any other custom highlights here
